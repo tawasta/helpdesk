89633b2766048ef160cf74be6560371f7c51980c
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2018-03-12 18:34:33 +0200

    [ADD] Customer can create issue from website. TODO: attachments

--- website_project_issue_form/__manifest__.py
+++ website_project_issue_form/__manifest__.py
@@ -22,8 +22,8 @@
 {
     'name': 'Website Project Issue Form',
     'summary': 'Issues can be submitted from website',
-    'version': '10.0.0.1.0',
-    'category': 'Tools',
+    'version': '10.0.0.1.1',
+    'category': 'website',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
--- website_project_issue_form/controllers/main.py
+++ website_project_issue_form/controllers/main.py
@@ -30,16 +30,42 @@ class WebsiteAccount(WebsiteAccount):
         """
         Route to create issues from website
         """
+        current_user = http.request.env.user
+        partner = current_user.partner_id
         if post:
             name = post.get('issue_name')
-            # Parse
+            # Parse HTML since description field is plain text
             description = BeautifulSoup(post.get('issue_summary'), 'lxml').text
+
+            # Find issue project with incoming mail server and alias
+            server_id = int(post.get('issue_email'))
+            email_inbox = http.request.env['fetchmail.server'].sudo().browse(
+                server_id).user.split('@')[0]
+            project_id = http.request.env['project.project'].sudo().search([
+                ('alias_name', '=', email_inbox),
+            ], limit=1).id
             values = {
                 'name': name,
                 'description': description,
-                'project_id': 2,
-                'partner_id': 8,
+                'project_id': project_id,
+                'partner_id': partner.id,
+                'email_from': partner.email,
             }
-            print values
-            http.request.env['project.issue'].sudo().create(values)
+            # Set default stage from project
+            issue = http.request.env['project.issue'].sudo(current_user).create(values)
+            issue.stage_id = issue.stage_find(project_id)
+            # Add partner as follower
+            notified_partner_ids = [partner.id]
+            issue.message_subscribe(notified_partner_ids)
+            # Send message to the thread
+            discussion_id = http.request.env.ref('mail.mt_comment').id
+
+            # TODO: Add attachments and form validation
+            issue.message_post(
+                subject=_("Issue created"),
+                message_type='comment',
+                subtype_id=discussion_id,
+                body=post.get('issue_summary'),
+                partner_ids=notified_partner_ids,
+            )
         return request.redirect('/my/issues')
