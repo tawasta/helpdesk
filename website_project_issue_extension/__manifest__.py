fa7772be00a9a88b4b56ba926dc32bd3a2c93149
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2018-03-19 07:46:46 +0200

    [ADD] Added system parameter for the module to work with newly created website_utilities functionalities

--- website_project_issue_extension/__manifest__.py
+++ website_project_issue_extension/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Website Project Issue Extension',
     'summary': 'Added functionality to Website Project Issue',
-    'version': '10.0.0.1.1',
+    'version': '10.0.0.1.2',
     'category': 'Tools',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
