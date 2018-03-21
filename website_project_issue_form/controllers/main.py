3cb0f31939a3c457b07d5f4329039ed0cfd180df
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2018-03-21 15:43:53 +0200

    [FIX] If issue's attachment exceeds attachment file maxsize, the message is returned instead of redirect

--- website_project_issue_form/controllers/main.py
+++ website_project_issue_form/controllers/main.py
@@ -86,16 +86,6 @@ class WebsiteAccount(WebsiteAccount):
                     'email_from': partner.email,
                     'user_id': None,
                 }
-                # Set default stage from project
-                issue = http.request.env['project.issue'].sudo(current_user).create(issue_values)
-                issue.stage_id = issue.stage_find(project_id)
-                values['id'] = issue.id
-                values['name'] = issue.name
-                values['stage'] = issue.stage_id.name
-
-                # Add partner as follower
-                notified_partner_ids = [partner.id]
-                issue.message_subscribe(notified_partner_ids)
 
                 # Check attachment isn't too big
                 attachment = post.get('issue_attachment') or None
@@ -109,14 +99,25 @@ class WebsiteAccount(WebsiteAccount):
 
                     if file_size > 20 * 1024 * 1024:
                         # File size too big
-                        return request.redirect('/my/issues')
+                        values['error'] = _('An error occured!')
+                        return json.dumps(values)
+                # Set default stage from project
+                issue = http.request.env['project.issue'].sudo(current_user).create(issue_values)
+                issue.stage_id = issue.stage_find(project_id)
+                values['id'] = issue.id
+                values['name'] = issue.name
+                values['stage'] = issue.stage_id.name
+
+                # Add partner as follower
+                notified_partner_ids = [partner.id]
+                issue.message_subscribe(notified_partner_ids)
 
                 # Send message to the thread
-                discussion_id = http.request.env.ref('mail.mt_comment').id
+                subtype_id = http.request.env.ref('mail.mt_comment').id
                 issue.message_post(
                     subject=_("Issue created"),
                     message_type='comment',
-                    subtype_id=discussion_id,
+                    subtype_id=subtype_id,
                     body=post.get('issue_summary'),
                     partner_ids=notified_partner_ids,
                     attachments=attachment_list,
