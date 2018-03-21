049efa62f9880574a1cf35eda7900d875cb40034
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2018-03-21 11:29:15 +0200

    [ADD] Added website_project_issue_extension to the bundle's manifest

--- helpdesk/__manifest__.py
+++ helpdesk/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Help Desk',
     'summary': 'Help Desk core bundle',
-    'version': '10.0.0.2.0',
+    'version': '10.0.0.3.0',
     'category': 'Tools',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
@@ -30,6 +30,7 @@
     'depends': [
         'project_issue_opportunity',
         'website_project_issue_form',
+        'website_project_issue_extension',
     ],
     'data': [
 
