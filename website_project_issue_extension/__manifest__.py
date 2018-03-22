11b1eb1beaed653782650cf2af660b4818776974
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2018-03-22 11:23:03 +0200

    [FIX] Changed noupdate to active

--- website_project_issue_extension/__manifest__.py
+++ website_project_issue_extension/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Website Project Issue Extension',
     'summary': 'Added functionality to Website Project Issue',
-    'version': '10.0.0.3.1',
+    'version': '10.0.0.3.2',
     'category': 'Tools',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
