884aec1194db0e202dabc97e8d9dd98cff04cc22
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2018-03-22 11:22:16 +0200

    [IMP] Changed less to css since the modifications are only a few and doesn't need to be added to assets

--- website_project_issue_form/__manifest__.py
+++ website_project_issue_form/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Website Project Issue Form',
     'summary': 'Issues can be submitted from website',
-    'version': '10.0.0.5.0',
+    'version': '10.0.0.5.1',
     'category': 'website',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
