215081c7d1c3e8aeb9ef068ca7f283b3eee0a846
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2018-03-29 18:49:35 +0300

    [FIX] Fixed id of project issue form view

--- project_issue_extension/__manifest__.py
+++ project_issue_extension/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Issue Extension',
     'summary': 'Modifications to Project Issue',
-    'version': '10.0.0.4.0',
+    'version': '10.0.0.4.1',
     'category': 'Project',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
