49e14bc8f06b9f49fe5a0d73f7cc52bf923b1c3e
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2018-10-26 14:59:08 +0300

    [ADD] JS version numbers

--- website_project_issue_extension/__manifest__.py
+++ website_project_issue_extension/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Website Project Issue Extension',
     'summary': 'Added functionality to Website Project Issue',
-    'version': '10.0.0.6.1',
+    'version': '10.0.0.6.2',
     'category': 'Website',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
