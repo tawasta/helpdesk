f81b75ad146bc3e409dc9591f364bbc43d2b3b04
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2019-01-18 15:51:31 +0200

    [ADD] Started to implement feature to edit followers on ongoing issue

--- website_project_issue_extension/__manifest__.py
+++ website_project_issue_extension/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Website Project Issue Extension',
     'summary': 'Added functionality to Website Project Issue',
-    'version': '10.0.0.6.4',
+    'version': '10.0.0.7.0',
     'category': 'Website',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
