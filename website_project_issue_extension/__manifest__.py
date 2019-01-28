dd8be1327775fa1ebcdb64e9e0820b9b4d444e96
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2019-01-28 14:09:55 +0200

    [IMP] If no attachments, hide dropdown

--- website_project_issue_extension/__manifest__.py
+++ website_project_issue_extension/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Website Project Issue Extension',
     'summary': 'Added functionality to Website Project Issue',
-    'version': '10.0.0.9.1',
+    'version': '10.0.0.9.2',
     'category': 'Website',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
