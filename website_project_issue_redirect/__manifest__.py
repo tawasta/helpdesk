e5a09c82c2b7da140437838bcc19d8083a15a7ee
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2019-03-25 15:31:10 +0200

    [ADD] Add redirect to issues

--- website_project_issue_redirect/__manifest__.py
+++ website_project_issue_redirect/__manifest__.py
@@ -20,26 +20,19 @@
 ##############################################################################
 
 {
-    'name': 'Help Desk',
-    'summary': 'Help Desk core bundle',
-    'version': '10.0.0.7.0',
-    'category': 'Tools',
+    'name': 'Website Project Issue Redirect',
+    'summary': 'Redirect to issues',
+    'version': '10.0.0.1.0',
+    'category': 'Website',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
     'depends': [
-        'project_issue_attachment',
-        'project_issue_email',
-        'project_issue_mattermost',
-        'project_issue_opportunity',
-        'project_issue_sheet_extension',
-        'project_issue_stage',
-        'website_project_issue_extension',
-        'website_project_issue_form',
-        'website_project_issue_hide_project',
-        'website_project_issue_redirect',
-        'mail_debrand',
+        'web',
     ],
-    'application': True,
+    'data': [
+        'views/login.xml',
+    ],
+    'application': False,
     'installable': True,
 }
