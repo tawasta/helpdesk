a539e920c95487796920537b65f82f5f835fee20
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2019-03-28 14:41:52 +0200

    [FIX] Fix record rules

--- project_issue_security/__manifest__.py
+++ project_issue_security/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Issue Security',
     'summary': 'Adds security group for portal users',
-    'version': '10.0.0.3.0',
+    'version': '10.0.0.3.1',
     'category': 'Project',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
