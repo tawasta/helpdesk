c22093f73f931e8740caf6d96a1b0f5ce9476ab2
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2019-04-02 15:10:44 +0300

    [FIX] Fix last_message in reply template to exclude notifications and add project_id as required field on form view

--- project_issue_email/__manifest__.py
+++ project_issue_email/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Issue Email',
     'summary': 'Email modifications to Project Issue',
-    'version': '10.0.0.11.0',
+    'version': '10.0.0.12.0',
     'category': 'Project',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
--- project_issue_email/models/res_partner.py
+++ project_issue_email/models/res_partner.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 
 # 1. Standard library imports:
-import re
 from datetime import datetime
 import pytz
 
 # 2. Known third party imports:
 
 # 3. Odoo imports (openerp):
-from odoo import api, fields, models, _
-from odoo import SUPERUSER_ID
+from odoo import api, models, _
 
 # 4. Imports from Odoo modules:
 
@@ -40,11 +38,18 @@ class ResPartner(models.Model):
         Use email_template when sending emails from system.
         We do this here since we don't want change the actual mail.message's
         body, just the outgoing email's.
+
+        @param body: message body
+        @param subject: message subject
+        @param recipients: message recipients
+        @param mail_values: Additional mail values that are passed to super
+        @return: super
         """
         mail_message_id = mail_values.get('mail_message_id', False)
         mail_message = self.env['mail.message'].browse(mail_message_id)
         body_html = mail_message.body
-        if mail_message and mail_message.model == 'project.issue' and mail_message.message_type != 'notification':
+        if mail_message and mail_message.model == 'project.issue' and \
+                mail_message.message_type != 'notification':
             issue = self.env[mail_message.model].browse(mail_message.res_id)
             settings = self.env['project.issue.settings'].sudo().search([
                 ('company_id', '=', issue.company_id.id),
@@ -53,15 +58,18 @@ class ResPartner(models.Model):
                 ('res_id', '=', issue.id),
                 ('model', '=', 'project.issue'),
                 ('subtype_id.internal', '=', False),
+                ('message_type', '!=', 'notification'),
                 ('id', '!=', mail_message_id),
             ], limit=1)
             email_values = dict()
             if last_message:
-                # If previous message in thread - send issue reply template with body and last message
+                # If previous message in thread,
+                # send issue reply template with body and last message
                 email_values = settings.email_issue_reply.generate_email(issue.id)
                 local = pytz.timezone('Europe/Helsinki')
-                create_date = datetime.strftime(pytz.utc.localize(datetime.strptime(
-                    last_message.create_date, '%Y-%m-%d %H:%M:%S')).astimezone(local), "%d.%m.%Y %H:%M:%S")
+                localized_time = pytz.utc.localize(datetime.strptime(
+                    last_message.create_date, '%Y-%m-%d %H:%M:%S')).astimezone(local)
+                create_date = datetime.strftime(localized_time, "%d.%m.%Y %H:%M:%S")
                 body_html += "<div style='margin-top:30px;padding-left:40px;border-left:solid 3px #ccc;'>"
                 body_html += "<h3>" + _("Previous message") + "</h3>"
                 body_html += _("Author: ") + last_message.author_id.name + ", " + create_date
