ddefa807da1ec5643f8e11dfb2f4cfd16caeae82
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2019-05-10 13:25:51 +0300

    [FIX] Add create permission

--- project_issue_security/__manifest__.py
+++ project_issue_security/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Issue Security',
     'summary': 'Adds security group for portal users',
-    'version': '10.0.0.5.0',
+    'version': '10.0.0.5.1',
     'category': 'Project',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
