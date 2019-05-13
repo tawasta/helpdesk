364df4f0e08819741e77d0a75da90e2ebad7c055
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2019-05-13 14:31:32 +0300

    [FIX] Updated translations

--- project_issue_email/__manifest__.py
+++ project_issue_email/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Issue Email',
     'summary': 'Email modifications to Project Issue',
-    'version': '10.0.0.15.4',
+    'version': '10.0.0.15.5',
     'category': 'Project',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
