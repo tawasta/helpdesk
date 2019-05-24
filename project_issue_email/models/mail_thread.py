238093d1b1d873d9a3417342fc274bd5a2e3be73
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2019-05-24 10:53:44 +0300

    [FIX] Fixed parsing from thunderbird

--- project_issue_email/models/mail_thread.py
+++ project_issue_email/models/mail_thread.py
@@ -108,26 +108,49 @@ class MailThread(models.AbstractModel):
             )
 
     @api.model
-    def message_parse(self, message, save_original=False):
-        """ Strip previous HTML messages from email """
-        res = super(MailThread, self).message_parse(message, save_original)
-        root = lxml.html.fromstring(res.get('body'))
+    def _message_extract_payload_postprocess(self, message, body, attachments):
+        """
+        Strip previous HTML messages from email
+
+        :param message: email message
+        :param body: body extracted from the email message
+        :param attachments: attachments related to email
+        :returns: body, attachments
+        """
+        body, attachments = super(MailThread, self)._message_extract_payload_postprocess(message, body, attachments)
+        if not body:
+            return body, attachments
+        root = lxml.html.fromstring(body)
+        postprocessed = False
         to_remove = []
         for node in root.iter():
-            # Remove blockquote from Gmail / Thunderbird / Mac email...
-            if node.tag == 'blockquote':
+            # Gmail / Mac email parsing
+            if node.tag == 'blockquote' and node.getprevious().get('class') != 'moz-cite-prefix':
+                postprocessed = True
                 parent = node.getparent()
                 to_remove.append(parent.getprevious())
                 to_remove.append(parent)
-            # Remove blockquote from Outlook
-            if node.tag == 'div' and node.get('id', '') == 'appendonsend':
+            # Gmail / Mac parsing ends
+            # Thunderbird parsing
+            if node.tag == 'blockquote' and node.getprevious().get('class') == 'moz-cite-prefix':
+                postprocessed = True
+                to_remove.append(node.getprevious())
+                to_remove.append(node)
+            # Thunderbird ends
+            # Outlook parsing: remove blockquote from Outlook
+            if node.tag == 'div' and node.get('id') == 'appendonsend':
+                postprocessed = True
                 to_remove.append(node)
                 to_remove.append(node.getnext())
             if node.tag == 'div' and node.get('id', '').endswith('divRplyFwdMsg'):
+                postprocessed = True
                 to_remove.append(node)
             if node.tag == 'div' and node.get('id', '').endswith('_issue_reply'):
+                postprocessed = True
                 to_remove.append(node)
+            # Outlook ends
         for node in to_remove:
             node.getparent().remove(node)
-        res['body'] = etree.tostring(root, pretty_print=False, encoding='UTF-8')
-        return res
+        if postprocessed:
+            body = etree.tostring(root, pretty_print=False, encoding='UTF-8')
+        return body, attachments
