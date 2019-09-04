35cd823391089ed4fe3c2ba4c18f53e81527a177
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2019-09-04 14:57:27 +0300

    [IMP] Use Core issue tree view instead of a custom one

--- project_issue_menu/__manifest__.py
+++ project_issue_menu/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Issue Menu',
     'summary': 'Add menu items for issues',
-    'version': '10.0.0.1.0',
+    'version': '10.0.0.2.0',
     'category': 'Project',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
