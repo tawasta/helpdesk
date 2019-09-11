b1f199d9969db0ee1fc123b0dd3c03e38a068df6
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2019-09-11 16:17:39 +0300

    [IMP] Security group type and category

--- project_issue_security/__manifest__.py
+++ project_issue_security/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Issue Security',
     'summary': 'Adds security group for portal users',
-    'version': '10.0.0.5.1',
+    'version': '10.0.0.6.0',
     'category': 'Project',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
