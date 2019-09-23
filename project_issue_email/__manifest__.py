a0df01f1b57e806b4980e4dbdbea63ebf2394b0b
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-09-23 10:31:42 +0300

    [IMP] Use logo shorthand url

--- project_issue_email/__manifest__.py
+++ project_issue_email/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Issue Email',
     'summary': 'Email modifications to Project Issue',
-    'version': '10.0.0.19.11',
+    'version': '10.0.0.19.12',
     'category': 'Project',
     'website': 'https://tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
