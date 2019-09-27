e0a9634abb33b878cde1918aabb62b68daae5757
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2019-09-27 13:07:53 +0300

    [FIX] Permission error when replying cross company issues

--- project_issue_email/__manifest__.py
+++ project_issue_email/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Issue Email',
     'summary': 'Email modifications to Project Issue',
-    'version': '10.0.0.19.15',
+    'version': '10.0.0.19.16',
     'category': 'Project',
     'website': 'https://tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
