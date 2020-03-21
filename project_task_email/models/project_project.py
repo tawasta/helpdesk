b3309237107046da7b612571fd2c24126f894ee1
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-03-21 16:24:04 +0200

    [MIG] 12.0 project_task_email

---
+++ project_task_email/models/project_project.py
@@ -0,0 +1,14 @@
+import logging
+from odoo import api, fields, models, _
+
+_logger = logging.getLogger(__name__)
+
+
+class ProjectProject(models.Model):
+
+    _inherit = 'project.project'
+
+    email_from = fields.Char(
+        string="Sender email",
+        help="Force sender email address on tasks",
+    )
