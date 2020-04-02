4e16ee0263487a0b48d284384367349ca35598ca
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-04-02 06:41:48 +0300

    [ADD] Button access to notification template

--- project_issue_email/__manifest__.py
+++ project_issue_email/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Issue Email',
     'summary': 'Email modifications to Project Issue',
-    'version': '10.0.0.19.18',
+    'version': '10.0.0.19.19',
     'category': 'Project',
     'website': 'https://tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
