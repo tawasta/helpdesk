2484fd600f1f22cafcde22833d2bc75f3bfb1675
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-04-02 06:50:10 +0300

    [ADD] Previous reply and original issue description

--- project_issue_email/__manifest__.py
+++ project_issue_email/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Project Issue Email',
     'summary': 'Email modifications to Project Issue',
-    'version': '10.0.0.19.19',
+    'version': '10.0.0.19.20',
     'category': 'Project',
     'website': 'https://tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
