b775d5df4d0475c136c14476d9d20d0fef827f42
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-07-19 16:19:19 +0300

    [MIG] 14.0 project_task_stage

---
+++ project_task_stage/__manifest__.py
@@ -0,0 +1,41 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2019- Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Project Task Stage",
+    "summary": "Add stages changes records to tasks",
+    "version": "14.0.1.2.3",
+    "category": "Project",
+    "website": "https://gitlab.com/tawasta/odoo/helpdesk",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "depends": [
+        "project",
+    ],
+    "data": [
+        "data/project_task_type.xml",
+        "security/ir.model.access.csv",
+        "views/project_task_form.xml",
+        "views/project_task_stage_change.xml",
+        "views/project_task_type.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
---
+++ project_task_stage/models/__init__.py
@@ -0,0 +1,4 @@
+from . import task_stage_change
+from . import project_task
+from . import project_project
+from . import project_task_type
