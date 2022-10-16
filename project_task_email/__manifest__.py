e5e01cec7789530bec9ac04f54dac81c6ea445b6
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-10-16 23:11:45 +0300

    [IMP] Remove duplicate logo

--- project_task_email/__manifest__.py
+++ project_task_email/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Project Task Email",
     "summary": "Email modifications to Project Task",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.1.1",
     "category": "Project",
     "website": "https://gitlab.com/tawasta/odoo/helpdesk",
     "author": "Tawasta",
